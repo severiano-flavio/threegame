09eeb2cfa9fa79f04626c93e58531e5735f21e5a
Flávio <severiano.flavio18@gmail.com>
2023-01-06 23:36:13 -0300

    configuração de projeto

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css';
+
+// Toggle commented out code to run alternate demo.
+import Client from './client';
+// import Demo from './Shader';
+
+window.addEventListener('DOMContentLoaded', () => {
+	Client();
+});
