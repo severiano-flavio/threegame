13b4a2c0ce698d8dfd71db50eb4429944d2e39ab
Flávio <severiano.flavio18@gmail.com>
2023-01-09 23:26:52 -0300

    refactor: refatora arquivos principais

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,35 @@
 import './style.css';
 
-// Toggle commented out code to run alternate demo.
-import Client from './client';
-// import Demo from './Shader';
+import THREEx3 from 'three-x3';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-window.addEventListener('DOMContentLoaded', () => {
-	Client();
-});
+import scene from './resources/scenes';
+
+import knight from './resources/character/knight';
+
+import * as THREE from 'three'
+
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight)
+camera.position.z = 5
+
+scene.add( knight );
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+document.querySelector<HTMLDivElement>('#app')!.appendChild(renderer.domElement);
+
+renderer.setAnimationLoop(() => {
+	knight.rotation.x += 0.01;
+	knight.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
+})
+
+const x3 = new THREEx3({
+THREE,
+OrbitControls,
+camera,
+renderer,
+scene
+})
